734ea35e6bc7640dcc13101abe9aaf759e3ee198
Herve Prot <herve.prot@symeos.com>
2011-11-27 18:36:02 +0100

    Fix error desincription mailing

--- htdocs/comm/mailing/fiche.php
+++ htdocs/comm/mailing/fiche.php
@@ -640,7 +640,7 @@ if ($_GET["action"] == 'create')
 	print '<td>';
 	// Editeur wysiwyg
 	require_once(DOL_DOCUMENT_ROOT."/lib/doleditor.class.php");
-        if(empty($_POST['body'])) $body='<br /><br /><br /><font face="Verdana, Helvetica, sans-serif" size="1">Pour ne plus recevoir de mailing, <a href="'.DOL_MAIN_URL_ROOT.'/public/mailing/desinscription?nom='.$conf->global->MAIN_INFO_SOCIETE_NOM.'&mail=__EMAIL__&id=__ID__'.'" target="_blank">désinscrivez vous</a> de la newsletter. (__EMAIL__) </font>';
+        if(empty($_POST['body'])) $body='<br /><br /><br /><font face="Verdana, Helvetica, sans-serif" size="1">Pour ne plus recevoir de mailing, <a href="'.DOL_MAIN_URL_ROOT.'/public/mailing/desinscription?nom='.$conf->global->MAIN_INFO_SOCIETE_NOM.'&mail=__EMAIL__'.'" target="_blank">désinscrivez vous</a> de la newsletter. (__EMAIL__) </font>';
         else $body=$_POST['body'];
 	$doleditor=new DolEditor('body',$body,'',320,'dolibarr_mailings','',true,true,$conf->fckeditor->enabled && $conf->global->FCKEDITOR_ENABLE_MAILING,20,70);
 	$doleditor->Create();
--- htdocs/public/mailing/desinscription.php
+++ htdocs/public/mailing/desinscription.php
@@ -1,6 +1,27 @@
-<!--  Copyright (C) 2011 Frédérique GUYOT       -->
-<!--  Copyright (C) 2011 Hervé Prot             -->
 <?php
+/* Copyright (C) 2011 Frédérique GUYOT
+ * Copyright (C) 2011 Herve Prot     <herve.prot@symeos.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *       \file       htdocs/public/mailing/desincription.php
+ *       \ingroup    mailing
+ *       \brief      Fiche mailing, desincription
+ *       \version    $Id: fiche.php,v 1.123 2011/11/25 00:46:33 hepr Exp $
+ */
 
 define("NOLOGIN",1);		// This means this output page does not require to be logged.
 define("NOCSRFCHECK",1);	// We accept to go on this page from external web site.
@@ -40,22 +61,6 @@ require("../../main.inc.php");
 	{
 		echo '<p>Variable mail non d&eacute;termin&eacute;e</p>';
 	}
-        // vérification du champ mail passé en paramétre dans l'url (email de l'entreprise)
-	if(isset($_GET['id']))		
-	{
-		if (is_string($_GET['id']))	
-		{
-			$id = stripslashes(htmlentities($_GET['id']));
-		}
-		else
-		{
-			echo '<p>L\'id n\'est pas de type string</p>';
-		}
-	}
-	else 
-	{
-		echo '<p>Variable id non d&eacute;termin&eacute;e</p>';
-	}
 
     /* Si le formulaire est envoyé alors on fait les traitements */
     if (isset($_POST['envoye']))
@@ -64,16 +69,14 @@ require("../../main.inc.php");
         
         $sql = "UPDATE ".MAIN_DB_PREFIX."societe SET";
 	$sql.= " newsletter=0";
-	$sql.= " WHERE rowid = ".$_GET['id'];
-        $sql.= " AND email = '".$_GET['mail']."'";
+	$sql.= " WHERE email = '".$_GET['mail']."'";
         
         if (!$db->query($sql) )
             $error++;
         
         $sql = "UPDATE ".MAIN_DB_PREFIX."socpeople SET";
 	$sql.= " newsletter=0";
-	$sql.= " WHERE rowid = ".$_GET['id'];
-        $sql.= " AND email = '".$_GET['mail']."'";
+	$sql.= " WHERE email = '".$_GET['mail']."'";
 
 	dol_syslog("Desincription::Update sql=".$sql,LOG_DEBUG);
 	if (!$db->query($sql) )
@@ -118,7 +121,7 @@ require("../../main.inc.php");
                     echo '<p style="color:red">'.$alert.'</p>';
                 }
                
-                print '<form action="desinscription.php?nom='.$nomEts.'&amp;mail='.$mail.'&amp;id='.$id.'"" method="post" id="formulaire" >';
+                print '<form action="desinscription.php?nom='.$nomEts.'&amp;mail='.$mail.'"" method="post" id="formulaire" >';
                     print '<p>Votre email : '.$mail.'</p>';
                     print '<p>';
                     print '</p>';
