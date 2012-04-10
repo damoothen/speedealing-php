0cb07f8848727494de54861ecb55783bff84e3ea
Herve Prot <herve.prot@symeos.com>
2012-04-10 17:55:41 +0200

    Fix error bills

--- htdocs/compta/facture.php
+++ htdocs/compta/facture.php
@@ -3132,7 +3132,7 @@ else
                 $genallowed=$user->rights->facture->creer;
                 $delallowed=$user->rights->facture->supprimer;
                 
-                print $filedir;exit;
+                //print $filedir;exit;
 
                 print '<br>';
                 print $formfile->showdocuments('facture',$filename,$filedir,$urlsource,$genallowed,$delallowed,$object->modelpdf,1,0,0,28,0,'','','',$soc->default_lang,$hookmanager);
