a6ceee632e10c26e7b12cf872c70918eddebd7e4
Regis Houssin <regis@dolibarr.fr>
2012-04-25 19:31:19 +0200

    Fix: remove conflict tag

--- htdocs/install/mysql/tables/llx_product_price.sql
+++ htdocs/install/mysql/tables/llx_product_price.sql
@@ -1,9 +1,8 @@
 -- ============================================================================
-<<<<<<< HEAD
 -- Copyright (C) 2002-2003	Rodolphe Quiedeville	<rodolphe@quiedeville.org>
 -- Copyright (C) 2005-2012	Regis Houssin			<regis@dolibarr.fr>
 -- Copyright (C) 2010		Juanjo Menent			<jmenent@2byte.es>
--- Copyright (C) 2011      Herve Prot           <herve.prot@symeos.com>
+-- Copyright (C) 2011		Herve Prot           <herve.prot@symeos.com>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
