bd113e21a3cded44d6cafd140a3cbb05725dfcec
Herve Prot <herve.prot@symeos.com>
2012-05-03 21:27:27 +0200

    Add a delete fonction

---
+++ htdocs/install/couchdb/delete_fonction.php
@@ -0,0 +1,62 @@
+<?php
+
+/* Copyright (C) 2012      Patrick Mary           <laube@hotmail.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * 	\file       htdocs/comm/serverprocess.php
+ * 	\ingroup    commercial societe
+ * 	\brief      load data to display
+ * 	\version    $Id: serverprocess.php,v 1.6 2012/01/27 16:15:05 synry63 Exp $
+ */
+require_once("../../main.inc.php");
+/* Array of database columns which should be read and sent back to DataTables. Use a space where
+ * you want to insert a non-database field (for example a counter or static image)
+ */
+
+
+$flush=1;
+if($flush)
+{
+    // reset old value
+    $result = $conf->couchdb->limit(50000)->getView('societe','target_id');
+    $i=0;
+    
+    if(count($result->rows)==0)
+    {
+        print "Effacement terminé";
+        exit;
+    }
+    
+    foreach ($result->rows AS $aRow)
+    {
+        $obj[$i]->_id=$aRow->value->_id;
+        $obj[$i]->_rev=$aRow->value->_rev;
+        $i++;
+    }
+
+    try {
+        $conf->couchdb->deleteDocs($obj);
+    } catch (Exception $e) {
+        echo "Something weird happened: ".$e->getMessage()." (errcode=".$e->getCode().")\n";
+        exit(1);
+    }
+
+    print "Effacement en cours";
+    exit;
+}
+
+?>
