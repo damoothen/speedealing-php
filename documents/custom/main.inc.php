694826efd50537ed68ff0c74c87d2d6c53fe5859
Herve Prot <herve.prot@symeos.com>
2012-10-12 10:28:48 +0200

    Remove old file

--- documents/custom/main.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-# Wrapper to include main into htdocs
-include_once('/opt/dolibarr/labdp/htdocs/main.inc.php');
-?>
--- documents/custom/master.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-# Wrapper to include master into htdocs
-include_once('/opt/dolibarr/labdp/htdocs/master.inc.php');
-?>
