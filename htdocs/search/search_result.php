599f6ca0e811930ecc247cec25c4edeecc6829b8
Herve Prot <herve.prot@symeos.com>
2012-10-28 12:25:27 +0100

    Modify error in search

--- htdocs/search/search_result.php
+++ htdocs/search/search_result.php
@@ -16,68 +16,67 @@
  */
 
 if (!defined('NOTOKENRENEWAL'))
-	define('NOTOKENRENEWAL', '1'); // Disables token renewal
+    define('NOTOKENRENEWAL', '1'); // Disables token renewal
 if (!defined('NOREQUIREMENU'))
-	define('NOREQUIREMENU', '1');
+    define('NOREQUIREMENU', '1');
 //if (! defined('NOREQUIREHTML'))  define('NOREQUIREHTML','1');
 if (!defined('NOREQUIREAJAX'))
-	define('NOREQUIREAJAX', '1');
+    define('NOREQUIREAJAX', '1');
 if (!defined('NOREQUIRESOC'))
-	define('NOREQUIRESOC', '1');
+    define('NOREQUIRESOC', '1');
 //if (! defined('NOREQUIRETRAN'))  define('NOREQUIRETRAN','1');
 
 require('../main.inc.php');
 require_once(DOL_DOCUMENT_ROOT . "/search/class/search.class.php");
 
 $object = new Search($db);
-$object->useDatabase('societe');
 
 $sParam = $_POST[search_item];
 
 try {
-	$result = $object->getIndexedView("list", array('limit' => 6,
-		'q' => $sParam ."*"
-			));
+    $result = $object->getIndexedView("list", array('limit' => 6,
+        'q' => $sParam . "*"
+            ));
 
-	$found = false;
+    $found = false;
 
-	if (isset($result->rows))
-		foreach ($result->rows AS $rowid => $aRow) {
-			foreach ($aRow->value as $key => $aCol) {
-				if ($key != "_id" && $key != "_rev" && is_string($aCol) && strpos(strtolower($aCol), strtolower($sParam)) !== false)
-					$found = true;
-			}
-			if (!$found) {
-				unset($result->rows[$rowid]);
-				$result->total_rows--;
-			}
-			$found = false;
-		}
+    if (isset($result->rows))
+        foreach ($result->rows AS $rowid => $aRow) {
+            foreach ($aRow->value as $key => $aCol) {
+                if ($key != "_id" && $key != "_rev" && is_string($aCol) && strpos(strtolower($aCol), strtolower($sParam)) !== false)
+                    $found = true;
+            }
+            if (!$found) {
+                unset($result->rows[$rowid]);
+                $result->total_rows--;
+            }
+            $found = false;
+        }
 
-	if ($result->total_rows)
-		$valuefounded = true;
+    if ($result->total_rows)
+        $valuefounded = true;
 } catch (Exception $e) {
-	
+    
 }
 ?>
 
 <div class="search_results search_pop">
-	<?php if ($valuefounded == true) { ?>
-		<h5 class="sepH_b">Showing <?php echo $result->total_rows; ?> results for <mark><?php echo $sParam; ?></mark></h5>
-		<ol>
-			<?php
-			if (isset($result->rows))
-				foreach ($result->rows AS $aRow) :
-					?>
-					<li>
-						<a href="societe/fiche.php?id=<?php echo $aRow->value->_id;?>"><?php echo $aRow->value->ThirdPartyName . " (" . $langs->trans($aRow->value->class) . ")"; ?></a>
-						<p><?php echo $aRow->value->Address; ?> <?php echo $aRow->value->Zip; ?> <?php echo $aRow->value->Town; ?></br>
-							<?php echo "RCS : ".$aRow->value->SIREN; ?> <?php echo $aRow->value->CustomerCode; ?></br>
-							<?php echo $aRow->value->Phone; ?> <?php echo $aRow->value->Mail; ?></p>
-					</li>
-				<?php endforeach; ?>
-		</ol>
-	<?php } else { ?>
-		Sorry no matches for <strong><?php echo $sParam; ?></strong>, please try some different term.
-	<?php }; ?>
+    <?php if ($valuefounded == true) { ?>
+        <h5 class="sepH_b">Showing <?php echo $result->total_rows; ?> results for <mark><?php echo $sParam; ?></mark></h5>
+        <ol>
+            <?php
+            if (isset($result->rows))
+                foreach ($result->rows AS $aRow) :
+                    ?>
+                    <li>
+                        <a href="societe/fiche.php?id=<?php echo $aRow->value->_id; ?>"><?php echo $aRow->value->name . " (" . $langs->trans($aRow->value->class) . ")"; ?></a>
+                        <p><?php echo $aRow->value->address; ?> <?php echo $aRow->value->Zip; ?> <?php echo $aRow->value->Town; ?></br>
+                            <?php echo "RCS : " . $aRow->value->idprof1; ?> <?php echo $aRow->value->CustomerCode; ?></br>
+                            <?php echo $aRow->value->phone; ?> <?php echo $aRow->value->Mail; ?></p>
+                    </li>
+                <?php endforeach; ?>
+        </ol>
+    <?php } else { ?>
+        Sorry no matches for <strong><?php echo $sParam; ?></strong>, please try some different term.
+    <?php }; ?>
 </div>
