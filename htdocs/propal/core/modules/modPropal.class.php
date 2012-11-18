d86845d4b9cd02f79037c86e086a71f400e51d55
David MOOTHEN <dmoothen@websitti.fr>
2012-11-19 00:49:00 +0100

    Rename module Propale to Propal (without e, for code compatibility).

--- htdocs/propal/core/modules/modPropal.class.php
+++ htdocs/propal/core/modules/modPropal.class.php
@@ -35,7 +35,7 @@ include_once DOL_DOCUMENT_ROOT . '/core/modules/DolibarrModules.class.php';
 /**
  * 	Classe de description et activation du module Propale
  */
-class modPropale extends DolibarrModules {
+class modPropal extends DolibarrModules {
 
     /**
      *   Constructor. Define names, constants, directories, boxes, permissions
@@ -105,7 +105,7 @@ class modPropale extends DolibarrModules {
 
         // Permissions
         $this->rights = array();
-        $this->rights_class = 'propale';
+        $this->rights_class = 'propal';
         $r = 0;
 
         $this->rights[$r]->id = 21; // id de la permission
@@ -154,12 +154,12 @@ class modPropale extends DolibarrModules {
         $this->menu = array();   // List of menus to add
         $r = 0;
         
-        $this->menus[$r]->_id = "menu:propale";
+        $this->menus[$r]->_id = "menu:propal";
         $this->menus[$r]->type = "top";
         $this->menus[$r]->position = 79;
         $this->menus[$r]->langs = "propal";
-        $this->menus[$r]->perms = '$user->rights->propale->lire';
-        $this->menus[$r]->enabled = '$conf->propale->enabled';
+        $this->menus[$r]->perms = '$user->rights->propal->lire';
+        $this->menus[$r]->enabled = '$conf->propal->enabled';
         $this->menus[$r]->usertype = 2;
         $this->menus[$r]->title = "Proposals";
         $r++;
@@ -168,30 +168,30 @@ class modPropale extends DolibarrModules {
         $this->menus[$r]->position = 790;
         $this->menus[$r]->url = "/propal/addpropal.php?action=create";
         $this->menus[$r]->langs = "propal";
-        $this->menus[$r]->perms = '$user->rights->propale->creer';
-        $this->menus[$r]->enabled = '$conf->propale->enabled';
+        $this->menus[$r]->perms = '$user->rights->propal->creer';
+        $this->menus[$r]->enabled = '$conf->propal->enabled';
         $this->menus[$r]->usertype = 2;
         $this->menus[$r]->title = "NewPropal";
-        $this->menus[$r]->fk_menu = "menu:propale";
+        $this->menus[$r]->fk_menu = "menu:propal";
         $r++;
         
         $this->menus[$r]->_id = "menu:list";
         $this->menus[$r]->position = 791;
         $this->menus[$r]->url = "/propal/list.php";
         $this->menus[$r]->langs = "propal";
-        $this->menus[$r]->perms = '$user->rights->propale->lire';
-        $this->menus[$r]->enabled = '$conf->propale->enabled';
+        $this->menus[$r]->perms = '$user->rights->propal->lire';
+        $this->menus[$r]->enabled = '$conf->propal->enabled';
         $this->menus[$r]->usertype = 2;
         $this->menus[$r]->title = "List";
-        $this->menus[$r]->fk_menu = "menu:propale";
+        $this->menus[$r]->fk_menu = "menu:propal";
         $r++;
         
         $this->menus[$r]->_id = "menu:drafts";
         $this->menus[$r]->position = 7910;
         $this->menus[$r]->url = "/propal/list.php";
         $this->menus[$r]->langs = "propal";
-        $this->menus[$r]->perms = '$user->rights->propale->lire';
-        $this->menus[$r]->enabled = '$conf->propale->enabled';
+        $this->menus[$r]->perms = '$user->rights->propal->lire';
+        $this->menus[$r]->enabled = '$conf->propal->enabled';
         $this->menus[$r]->usertype = 2;
         $this->menus[$r]->title = "PropalsDraft";
         $this->menus[$r]->fk_menu = "menu:list";
@@ -201,8 +201,8 @@ class modPropale extends DolibarrModules {
         $this->menus[$r]->position = 7911;
         $this->menus[$r]->url = "/propal/list.php";
         $this->menus[$r]->langs = "propal";
-        $this->menus[$r]->perms = '$user->rights->propale->lire';
-        $this->menus[$r]->enabled = '$conf->propale->enabled';
+        $this->menus[$r]->perms = '$user->rights->propal->lire';
+        $this->menus[$r]->enabled = '$conf->propal->enabled';
         $this->menus[$r]->usertype = 2;
         $this->menus[$r]->title = "PropalsOpened";
         $this->menus[$r]->fk_menu = "menu:list";
@@ -212,8 +212,8 @@ class modPropale extends DolibarrModules {
         $this->menus[$r]->position = 7912;
         $this->menus[$r]->url = "/propal/list.php";
         $this->menus[$r]->langs = "propal";
-        $this->menus[$r]->perms = '$user->rights->propale->lire';
-        $this->menus[$r]->enabled = '$conf->propale->enabled';
+        $this->menus[$r]->perms = '$user->rights->propal->lire';
+        $this->menus[$r]->enabled = '$conf->propal->enabled';
         $this->menus[$r]->usertype = 2;
         $this->menus[$r]->title = "PropalStatusSigned";
         $this->menus[$r]->fk_menu = "menu:list";
@@ -223,8 +223,8 @@ class modPropale extends DolibarrModules {
         $this->menus[$r]->position = 7913;
         $this->menus[$r]->url = "/propal/list.php";
         $this->menus[$r]->langs = "propal";
-        $this->menus[$r]->perms = '$user->rights->propale->lire';
-        $this->menus[$r]->enabled = '$conf->propale->enabled';
+        $this->menus[$r]->perms = '$user->rights->propal->lire';
+        $this->menus[$r]->enabled = '$conf->propal->enabled';
         $this->menus[$r]->usertype = 2;
         $this->menus[$r]->title = "PropalStatusNotSigned";
         $this->menus[$r]->fk_menu = "menu:list";
@@ -234,8 +234,8 @@ class modPropale extends DolibarrModules {
         $this->menus[$r]->position = 7914;
         $this->menus[$r]->url = "/propal/list.php";
         $this->menus[$r]->langs = "propal";
-        $this->menus[$r]->perms = '$user->rights->propale->lire';
-        $this->menus[$r]->enabled = '$conf->propale->enabled';
+        $this->menus[$r]->perms = '$user->rights->propal->lire';
+        $this->menus[$r]->enabled = '$conf->propal->enabled';
         $this->menus[$r]->usertype = 2;
         $this->menus[$r]->title = "PropalStatusBilled";
         $this->menus[$r]->fk_menu = "menu:list";
@@ -245,32 +245,32 @@ class modPropale extends DolibarrModules {
         $this->menus[$r]->position = 792;
         $this->menus[$r]->url = "/propal/stats/index.php";
         $this->menus[$r]->langs = "propal";
-        $this->menus[$r]->perms = '$user->rights->propale->lire';
-        $this->menus[$r]->enabled = '$conf->propale->enabled';
+        $this->menus[$r]->perms = '$user->rights->propal->lire';
+        $this->menus[$r]->enabled = '$conf->propal->enabled';
         $this->menus[$r]->usertype = 2;
         $this->menus[$r]->title = "Statistics";
-        $this->menus[$r]->fk_menu = "menu:propale";
+        $this->menus[$r]->fk_menu = "menu:propal";
         $r++;
         
         // Exports
         //--------
-        $r = 0;
-
-        $r++;
-        $this->export_code[$r] = $this->rights_class . '_' . $r;
-        $this->export_label[$r] = 'ProposalsAndProposalsLines'; // Translation key (used only if key ExportDataset_xxx_z not found)
-        $this->export_permission[$r] = array(array("propale", "export"));
-        $this->export_fields_array[$r] = array('s.rowid' => "IdCompany", 's.nom' => 'CompanyName', 's.address' => 'Address', 's.cp' => 'Zip', 's.ville' => 'Town', 'cp.code' => 'Country', 's.tel' => 'Phone', 's.siren' => 'ProfId1', 's.siret' => 'ProfId2', 's.ape' => 'ProfId3', 's.idprof4' => 'ProfId4', 'c.rowid' => "Id", 'c.ref' => "Ref", 'c.ref_client' => "RefCustomer", 'c.fk_soc' => "IdCompany", 'c.datec' => "DateCreation", 'c.datep' => "DatePropal", 'c.fin_validite' => "DateEndPropal", 'c.remise_percent' => "GlobalDiscount", 'c.total_ht' => "TotalHT", 'c.total' => "TotalTTC", 'c.fk_statut' => 'Status', 'c.note' => "Note", 'c.date_livraison' => 'DeliveryDate', 'cd.rowid' => 'LineId', 'cd.label' => "Label", 'cd.description' => "LineDescription", 'cd.product_type' => 'TypeOfLineServiceOrProduct', 'cd.tva_tx' => "LineVATRate", 'cd.qty' => "LineQty", 'cd.total_ht' => "LineTotalHT", 'cd.total_tva' => "LineTotalVAT", 'cd.total_ttc' => "LineTotalTTC", 'p.rowid' => 'ProductId', 'p.ref' => 'ProductRef', 'p.label' => 'ProductLabel');
-        $this->export_entities_array[$r] = array('s.rowid' => "company", 's.nom' => 'company', 's.address' => 'company', 's.cp' => 'company', 's.ville' => 'company', 'cp.code' => 'company', 's.tel' => 'company', 's.siren' => 'company', 's.ape' => 'company', 's.idprof4' => 'company', 's.siret' => 'company', 'c.rowid' => "propal", 'c.ref' => "propal", 'c.ref_client' => "propal", 'c.fk_soc' => "propal", 'c.datec' => "propal", 'c.datep' => "propal", 'c.fin_validite' => "propal", 'c.remise_percent' => "propal", 'c.total_ht' => "propal", 'c.total' => "propal", 'c.fk_statut' => "propal", 'c.note' => "propal", 'c.date_livraison' => "propal", 'cd.rowid' => 'propal_line', 'cd.label' => "propal_line", 'cd.description' => "propal_line", 'cd.product_type' => 'propal_line', 'cd.tva_tx' => "propal_line", 'cd.qty' => "propal_line", 'cd.total_ht' => "propal_line", 'cd.total_tva' => "propal_line", 'cd.total_ttc' => "propal_line", 'p.rowid' => 'product', 'p.ref' => 'product', 'p.label' => 'product');
-        $this->export_dependencies_array[$r] = array('propal_line' => 'cd.rowid', 'product' => 'cd.rowid'); // To add unique key if we ask a field of a child to avoid the DISTINCT to discard them
-
-        $this->export_sql_start[$r] = 'SELECT DISTINCT ';
-        $this->export_sql_end[$r] = ' FROM ' . MAIN_DB_PREFIX . 'societe as s ';
-        $this->export_sql_end[$r] .=' LEFT JOIN ' . MAIN_DB_PREFIX . 'c_pays as cp ON s.fk_pays = cp.rowid,';
-        $this->export_sql_end[$r] .=' ' . MAIN_DB_PREFIX . 'propal as c, ' . MAIN_DB_PREFIX . 'propaldet as cd';
-        $this->export_sql_end[$r] .=' LEFT JOIN ' . MAIN_DB_PREFIX . 'product as p on (cd.fk_product = p.rowid)';
-        $this->export_sql_end[$r] .=' WHERE c.fk_soc = s.rowid AND c.rowid = cd.fk_propal';
-        $this->export_sql_end[$r] .=' AND c.entity = ' . $conf->entity;
+//        $r = 0;
+//
+//        $r++;
+//        $this->export_code[$r] = $this->rights_class . '_' . $r;
+//        $this->export_label[$r] = 'ProposalsAndProposalsLines'; // Translation key (used only if key ExportDataset_xxx_z not found)
+//        $this->export_permission[$r] = array(array("propale", "export"));
+//        $this->export_fields_array[$r] = array('s.rowid' => "IdCompany", 's.nom' => 'CompanyName', 's.address' => 'Address', 's.cp' => 'Zip', 's.ville' => 'Town', 'cp.code' => 'Country', 's.tel' => 'Phone', 's.siren' => 'ProfId1', 's.siret' => 'ProfId2', 's.ape' => 'ProfId3', 's.idprof4' => 'ProfId4', 'c.rowid' => "Id", 'c.ref' => "Ref", 'c.ref_client' => "RefCustomer", 'c.fk_soc' => "IdCompany", 'c.datec' => "DateCreation", 'c.datep' => "DatePropal", 'c.fin_validite' => "DateEndPropal", 'c.remise_percent' => "GlobalDiscount", 'c.total_ht' => "TotalHT", 'c.total' => "TotalTTC", 'c.fk_statut' => 'Status', 'c.note' => "Note", 'c.date_livraison' => 'DeliveryDate', 'cd.rowid' => 'LineId', 'cd.label' => "Label", 'cd.description' => "LineDescription", 'cd.product_type' => 'TypeOfLineServiceOrProduct', 'cd.tva_tx' => "LineVATRate", 'cd.qty' => "LineQty", 'cd.total_ht' => "LineTotalHT", 'cd.total_tva' => "LineTotalVAT", 'cd.total_ttc' => "LineTotalTTC", 'p.rowid' => 'ProductId', 'p.ref' => 'ProductRef', 'p.label' => 'ProductLabel');
+//        $this->export_entities_array[$r] = array('s.rowid' => "company", 's.nom' => 'company', 's.address' => 'company', 's.cp' => 'company', 's.ville' => 'company', 'cp.code' => 'company', 's.tel' => 'company', 's.siren' => 'company', 's.ape' => 'company', 's.idprof4' => 'company', 's.siret' => 'company', 'c.rowid' => "propal", 'c.ref' => "propal", 'c.ref_client' => "propal", 'c.fk_soc' => "propal", 'c.datec' => "propal", 'c.datep' => "propal", 'c.fin_validite' => "propal", 'c.remise_percent' => "propal", 'c.total_ht' => "propal", 'c.total' => "propal", 'c.fk_statut' => "propal", 'c.note' => "propal", 'c.date_livraison' => "propal", 'cd.rowid' => 'propal_line', 'cd.label' => "propal_line", 'cd.description' => "propal_line", 'cd.product_type' => 'propal_line', 'cd.tva_tx' => "propal_line", 'cd.qty' => "propal_line", 'cd.total_ht' => "propal_line", 'cd.total_tva' => "propal_line", 'cd.total_ttc' => "propal_line", 'p.rowid' => 'product', 'p.ref' => 'product', 'p.label' => 'product');
+//        $this->export_dependencies_array[$r] = array('propal_line' => 'cd.rowid', 'product' => 'cd.rowid'); // To add unique key if we ask a field of a child to avoid the DISTINCT to discard them
+//
+//        $this->export_sql_start[$r] = 'SELECT DISTINCT ';
+//        $this->export_sql_end[$r] = ' FROM ' . MAIN_DB_PREFIX . 'societe as s ';
+//        $this->export_sql_end[$r] .=' LEFT JOIN ' . MAIN_DB_PREFIX . 'c_pays as cp ON s.fk_pays = cp.rowid,';
+//        $this->export_sql_end[$r] .=' ' . MAIN_DB_PREFIX . 'propal as c, ' . MAIN_DB_PREFIX . 'propaldet as cd';
+//        $this->export_sql_end[$r] .=' LEFT JOIN ' . MAIN_DB_PREFIX . 'product as p on (cd.fk_product = p.rowid)';
+//        $this->export_sql_end[$r] .=' WHERE c.fk_soc = s.rowid AND c.rowid = cd.fk_propal';
+//        $this->export_sql_end[$r] .=' AND c.entity = ' . $conf->entity;
     }
 
     /**
