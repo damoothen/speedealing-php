7734a843ea4daf8c7ac101b85dee0174fdca02a0
David MOOTHEN <dmoothen@websitti.fr>
2012-11-19 13:13:15 +0100

    Updating propals list.

--- htdocs/propal/list.php
+++ htdocs/propal/list.php
@@ -1,11 +1,15 @@
 <?php
-/* Copyright (C) 2001-2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
- * Copyright (C) 2003      Eric Seigne          <erics@rycks.com>
- * Copyright (C) 2004-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2005-2012 Regis Houssin        <regis@dolibarr.fr>
- * Copyright (C) 2010-2012 Herve Prot           <herve.prot@symeos.com>
- * Copyright (C) 2012      David Moothen        <dmoothen@websitti.fr>
- *
+/* Copyright (C) 2001-2007 Rodolphe Quiedeville  <rodolphe@quiedeville.org>
+ * Copyright (C) 2004-2011 Laurent Destailleur   <eldy@users.sourceforge.net>
+ * Copyright (C) 2004      Eric Seigne           <eric.seigne@ryxeo.com>
+ * Copyright (C) 2005      Marc Barilley / Ocebo <marc@ocebo.com>
+ * Copyright (C) 2005-2012 Regis Houssin         <regis@dolibarr.fr>
+ * Copyright (C) 2006      Andre Cianfarani      <acianfa@free.fr>
+ * Copyright (C) 2010-2011 Juanjo Menent         <jmenent@2byte.es>
+ * Copyright (C) 2010-2011 Philippe Grand        <philippe.grand@atoo-net.com>
+ * Copyright (C) 2012      Christophe Battarel   <christophe.battarel@altairis.fr>
+ * Copyright (C) 2012      David Moothen   <dmoothen@websitti.fr>
+*
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -20,41 +24,65 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ *	\file       	htdocs/comm/propal.php
+ *	\ingroup    	propale
+ *	\brief      	Page of commercial proposals card and list
+ */
+
 require("../main.inc.php");
 require_once(DOL_DOCUMENT_ROOT . "/contact/class/contact.class.php");
-require_once(DOL_DOCUMENT_ROOT . "/agenda/class/agenda.class.php");
+require_once(DOL_DOCUMENT_ROOT . "/propal/class/propal.class.php");
 require_once(DOL_DOCUMENT_ROOT . "/core/lib/date.lib.php");
 require_once(DOL_DOCUMENT_ROOT . "/agenda/lib/agenda.lib.php");
 if (!empty($conf->projet->enabled))
     require_once(DOL_DOCUMENT_ROOT . "/core/lib/project.lib.php");
 
-$langs->load("companies");
-$langs->load("agenda");
-$langs->load("commercial");
+$langs->load('companies');
+$langs->load('propal');
+$langs->load('compta');
+$langs->load('bills');
+$langs->load('orders');
+$langs->load('products');
 
-$showbirthday = empty($conf->use_javascript_ajax) ? GETPOST("showbirthday", "int") : 1;
+$socid=GETPOST('socid','int');
+
+$search_user=GETPOST('search_user','int');
+$search_sale=GETPOST('search_sale','int');
+$search_ref=GETPOST('sf_ref')?GETPOST('sf_ref','alpha'):GETPOST('search_ref','alpha');
+$search_refcustomer=GETPOST('search_refcustomer','alpha');
+$search_societe=GETPOST('search_societe','alpha');
+$search_montant_ht=GETPOST('search_montant_ht','alpha');
+
+$sall=GETPOST("sall");
+$mesg=(GETPOST("msg") ? GETPOST("msg") : GETPOST("mesg"));
+$year=GETPOST("year");
+$month=GETPOST("month");
+
+// Nombre de ligne pour choix de produit/service predefinis
+$NBLINES=4;
 
 // Security check
-$socid = GETPOST("socid", 'alpha');
-if ($user->societe_id)
-    $socid = $user->societe_id;
-$result = restrictedArea($user, 'agenda', 0, '', 'myactions');
+//$socid = GETPOST("socid", 'alpha');
+//if ($user->societe_id)
+//    $socid = $user->societe_id;
+//$result = restrictedArea($user, 'agenda', 0, '', 'myactions');
 
 $canedit = 1;
-if (!$user->rights->agenda->myactions->read)
-    accessforbidden();
-if (!$user->rights->agenda->allactions->read)
-    $canedit = 0;
-if (!$user->rights->agenda->allactions->read || $filter == 'mine') { // If no permission to see all, we show only affected to me
-    $filtera = $user->id;
-    $filtert = $user->id;
-    $filterd = $user->id;
-}
+//if (!$user->rights->agenda->myactions->read)
+//    accessforbidden();
+//if (!$user->rights->agenda->allactions->read)
+//    $canedit = 0;
+//if (!$user->rights->agenda->allactions->read || $filter == 'mine') { // If no permission to see all, we show only affected to me
+//    $filtera = $user->id;
+//    $filtert = $user->id;
+//    $filterd = $user->id;
+//}
 
 $type = GETPOST('type');
-$all = (GETPOST('all') && $user->rights->agenda->allactions->read);
+$all = (GETPOST('all') && $user->rights->propale>lire);
 
-$object = new Agenda($db);
+$object = new Propal($db);
 
 if (!empty($_GET['json'])) {
     $output = array(
@@ -64,15 +92,15 @@ if (!empty($_GET['json'])) {
         "aaData" => array()
     );
 
-    $keystart[0] = $user->id;
-    $keyend[0] = $user->id;
-    $keyend[1] = new stdClass();
+//    $keystart[0] = $user->id;
+//    $keyend[0] = $user->id;
+//    $keyend[1] = new stdClass();
 
     /* $params = array('startkey' => array($user->id, mktime(0, 0, 0, date("m") - 1, date("d"), date("Y"))),
       'endkey' => array($user->id, mktime(0, 0, 0, date("m") + 1, date("d"), date("Y")))); */
 
     try {
-        $result = $object->getView($_GET["json"], array('startkey' => $keystart, 'endkey' => $keyend));
+        $result = $object->getView($_GET["json"]);
     } catch (Exception $exc) {
         print $exc->getMessage();
     }
@@ -106,7 +134,7 @@ $userstatic = new User($db);
 $now = dol_now();
 
 $help_url = 'EN:Module_Agenda_En|FR:Module_Agenda|ES:M&omodulodulo_Agenda';
-llxHeader('', $langs->trans("Agenda"), $help_url);
+llxHeader('', $langs->trans("Proposals"), $help_url);
 
 $title = $langs->trans("DoneAndToDoActions");
 if ($type == 'DONE')
@@ -123,49 +151,7 @@ if ($socid) {
 }
 
 
-print_fiche_titre($newtitle);
-?>
-<div class="dashboard">
-    <div class="columns">
-        <div class="nine-columns twelve-columns-mobile graph">
-            <?php echo $object->graphEisenhower(); ?>
-        </div>
-
-        <div class="three-columns twelve-columns-mobile new-row-mobile">
-            <ul class="stats split-on-mobile">
-                <li>
-                    <?php
-                    $agenda = new Agenda($db);
-                    $result = $agenda->getView("countTODO", array("group" => true, "key" => $user->id));
-                    print '<strong>' . (int) $result->rows[0]->value . '</strong>';
-                    print $langs->trans('NewActions');
-                    ?>
-                </li>
-                <li>
-                    <?php
-                    $result = $agenda->getView("countON", array("group" => true, "key" => $user->id));
-                    print '<strong>' . (int) $result->rows[0]->value . '</strong>';
-                    print $langs->trans('DoActions');
-                    ?>
-                </li>
-                <li><?php
-                    $result = $agenda->getView("countByUser", array("group" => true, "group_level" => "none", "startkey" => array($user->id, mktime(0, 0, 0, 1, 1, date(Y))), "endkey" => array($user->id, new stdClass())));
-                    print '<strong>' . (int) $result->rows[0]->value . '</strong>';
-                    print $langs->trans('SumMyActions');
-                    ?>
-                </li>
-                <li>
-                    <?php
-                    $result = $agenda->getView("count", array("group" => true, "group_level" => "none", "startkey" => mktime(0, 0, 0, 1, 1, date(Y)), "endkey" => new stdClass()));
-                    print '<strong>' . (int) $result->rows[0]->value . '</strong>';
-                    print $langs->trans('SumActions');
-                    ?>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
-<?php
+print_fiche_titre($langs->trans("Proposals"));
 print '<div class="with-padding">';
 
 /*
@@ -173,17 +159,17 @@ print '<div class="with-padding">';
  *
  */
 
-if ($user->rights->agenda->myactions->create || $user->rights->agenda->allactions->create) {
+if ($user->rights->propal->creer) {
     print '<p class="button-height right">';
     print '<span class="button-group">';
-    print '<a class="button icon-star" href="' . strtolower(get_class($object)) . '/fiche.php?action=create">' . $langs->trans("NewAction") . '</a>';
+    print '<a class="button icon-star" href="' . strtolower(get_class($object)) . '/addpropal.php?action=create">' . $langs->trans("NewPropal") . '</a>';
     print "</span>";
     print "</p>";
 }
 
 $i = 0;
 $obj = new stdClass();
-print '<table class="display dt_act" id="listactions" >';
+print '<table class="display dt_act" id="listpropals" >';
 // Ligne des titres 
 print'<thead>';
 print'<tr>';
@@ -195,22 +181,12 @@ $obj->aoColumns[$i]->bSearchable = false;
 $obj->aoColumns[$i]->bVisible = false;
 $i++;
 print'<th class="essential">';
-print $langs->trans("Titre");
+print $langs->trans("Ref");
 print'</th>';
-$obj->aoColumns[$i]->mDataProp = "label";
+$obj->aoColumns[$i]->mDataProp = "ref";
 $obj->aoColumns[$i]->bUseRendered = false;
 $obj->aoColumns[$i]->bSearchable = true;
-$obj->aoColumns[$i]->fnRender = $object->datatablesFnRender("label", "url");
-$i++;
-print'<th class="essential">';
-print $langs->trans('DateEchAction');
-print'</th>';
-$obj->aoColumns[$i]->mDataProp = "datep";
-$obj->aoColumns[$i]->sClass = "center";
-$obj->aoColumns[$i]->sDefaultContent = "";
-$obj->aoColumns[$i]->bUseRendered = false;
-$obj->aoColumns[$i]->fnRender = $object->datatablesFnRender("datep", "datetime");
-//$obj->aoColumns[$i]->sClass = "edit";
+$obj->aoColumns[$i]->fnRender = $object->datatablesFnRender("ref", "url", array('url' => 'propal/propal.php?id='));
 $i++;
 print'<th class="essential">';
 print $langs->trans('Company');
@@ -220,41 +196,54 @@ $obj->aoColumns[$i]->sDefaultContent = "";
 $obj->aoColumns[$i]->fnRender = $societe->datatablesFnRender("societe.name", "url", array('id' => "societe.id"));
 $i++;
 print'<th class="essential">';
-print $langs->trans('Contact');
+print $langs->trans("RefCustomer");
 print'</th>';
-$obj->aoColumns[$i]->mDataProp = "contact.name";
-$obj->aoColumns[$i]->sDefaultContent = "";
-$obj->aoColumns[$i]->fnRender = $contact->datatablesFnRender("contact.name", "url", array('id' => "contact.id"));
+$obj->aoColumns[$i]->mDataProp = "ref_client";
+$obj->aoColumns[$i]->bUseRendered = false;
+$obj->aoColumns[$i]->bSearchable = true;
 $i++;
-/* print'<th class="essential">';
+print'<th class="essential">';
+print $langs->trans('Date');
+print'</th>';
+$obj->aoColumns[$i]->mDataProp = "date";
+$obj->aoColumns[$i]->sClass = "center";
+$obj->aoColumns[$i]->sDefaultContent = "";
+$obj->aoColumns[$i]->bUseRendered = false;
+$obj->aoColumns[$i]->fnRender = $object->datatablesFnRender("date", "datetime");
+//$obj->aoColumns[$i]->sClass = "edit";
+$i++;
+print'<th class="essential">';
+print $langs->trans('DateEnd');
+print'</th>';
+$obj->aoColumns[$i]->mDataProp = "date_livraison";
+$obj->aoColumns[$i]->sClass = "center";
+$obj->aoColumns[$i]->sDefaultContent = "";
+$obj->aoColumns[$i]->bUseRendered = false;
+$obj->aoColumns[$i]->fnRender = $object->datatablesFnRender("date_livraison", "datetime");
+//$obj->aoColumns[$i]->sClass = "edit";
+$i++;
+
+//print'<th class="essential">';
+//print $langs->trans('Contact');
+//print'</th>';
+//$obj->aoColumns[$i]->mDataProp = "contact.name";
+//$obj->aoColumns[$i]->sDefaultContent = "";
+//$obj->aoColumns[$i]->fnRender = $contact->datatablesFnRender("contact.name", "url", array('id' => "contact.id"));
+//$i++;
+ print'<th class="essential">';
   print $langs->trans('ActionUserAsk');
   print'</th>';
   $obj->aoColumns[$i]->mDataProp = "author";
   $obj->aoColumns[$i]->sDefaultContent = "";
-  $obj->aoColumns[$i]->fnRender = $userstatic->datatablesFnRender("author.name", "url", array('id' => "author.id"));
-  $i++; */
-print'<th class="essential">';
-print $langs->trans('AffectedTo');
-print'</th>';
-$obj->aoColumns[$i]->mDataProp = "usertodo";
-$obj->aoColumns[$i]->sClass = "dol_select";
-$obj->aoColumns[$i]->sDefaultContent = "";
-$obj->aoColumns[$i]->fnRender = $userstatic->datatablesFnRender("usertodo.name", "url", array('id' => "usertodo.id"));
-$i++;
-print'<th class="essential">';
-print $langs->trans('DoneBy');
-print'</th>';
-$obj->aoColumns[$i]->mDataProp = "userdone";
-$obj->aoColumns[$i]->sDefaultContent = "";
-$obj->aoColumns[$i]->fnRender = $userstatic->datatablesFnRender("userdone.name", "url", array('id' => "userdone.id"));
-$i++;
+  //$obj->aoColumns[$i]->fnRender = $userstatic->datatablesFnRender("author.name", "url", array('id' => "author.id"));
+  $i++; 
 print'<th class="essential">';
 print $langs->trans("Status");
 print'</th>';
 $obj->aoColumns[$i]->mDataProp = "Status";
 $obj->aoColumns[$i]->sClass = "center";
-$obj->aoColumns[$i]->sDefaultContent = "TODO";
-$obj->aoColumns[$i]->fnRender = $object->datatablesFnRender("Status", "status", array("dateEnd" => "datep"));
+$obj->aoColumns[$i]->sDefaultContent = "DRAFT";
+$obj->aoColumns[$i]->fnRender = $object->datatablesFnRender("Status", "status");
 $i++;
 print'<th class="essential">';
 print $langs->trans('Action');
@@ -265,7 +254,7 @@ $obj->aoColumns[$i]->sWidth = "60px";
 $obj->aoColumns[$i]->bSortable = false;
 $obj->aoColumns[$i]->sDefaultContent = "";
 
-$url = "agenda/fiche.php";
+$url = "propal/propal.php";
 $obj->aoColumns[$i]->fnRender = 'function(obj) {
 	var ar = [];
 	ar[ar.length] = "<a href=\"' . $url . '?id=";
@@ -284,22 +273,20 @@ $i = 0; //Doesn't work with bServerSide
 print'<tr>';
 print'<th id="' . $i . '"></th>';
 $i++;
-print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search Name") . '" /></th>';
-$i++;
-print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search Date") . '" /></th>';
+print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search Ref") . '" /></th>';
 $i++;
 print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search Company") . '" /></th>';
 $i++;
-print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search Contact") . '" /></th>';
+print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search RefCustomer") . '" /></th>';
 $i++;
-/* print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search author") . '" /></th>';
-  $i++; */
-print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search usertodo") . '" /></th>';
+print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search Date") . '" /></th>';
 $i++;
-print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search userdone") . '" /></th>';
+print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search DateEnd") . '" /></th>';
 $i++;
-print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search Status") . '" /></th>';
-$i++;
+print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search author") . '" /></th>';
+$i++; 
+//print'<th id="' . $i . '"><input type="text" placeholder="' . $langs->trans("Search Status") . '" /></th>';
+//$i++;
 print'<th id="' . $i . '"></th>';
 $i++;
 print'</tr>';
@@ -312,19 +299,21 @@ print "</table>";
 $obj->aaSorting = array(array(2, 'asc'));
 //$obj->bServerSide = true;
 
-if ($all) {
-    if ($type == "DONE")
-        $obj->sAjaxSource = "core/ajax/listdatatables.php?json=actionsDONE&class=" . get_class($object);
-    else
-        $obj->sAjaxSource = "core/ajax/listdatatables.php?json=actionsTODO&class=" . get_class($object);
-} else {
-    if ($type == "DONE")
-        $obj->sAjaxSource = $_SERVER["PHP_SELF"] . "?json=listDONEByUser";
-    else
-        $obj->sAjaxSource = $_SERVER["PHP_SELF"] . "?json=listTODOByUser";
-}
+//if ($all) {
+//    if ($type == "DONE")
+//        $obj->sAjaxSource = "core/ajax/listdatatables.php?json=actionsDONE&class=" . get_class($object);
+//    else
+//        $obj->sAjaxSource = "core/ajax/listdatatables.php?json=actionsTODO&class=" . get_class($object);
+//} else {
+//    if ($type == "DONE")
+//        $obj->sAjaxSource = $_SERVER["PHP_SELF"] . "?json=listDONEByUser";
+//    else
+//        $obj->sAjaxSource = $_SERVER["PHP_SELF"] . "?json=listTODOByUser";
+//        
+//}
+$obj->sAjaxSource = $_SERVER["PHP_SELF"] . "?json=list";
 
-$object->datatablesCreate($obj, "listactions", true, true);
+$object->datatablesCreate($obj, "listpropals", true, true);
 print '</div>'; // end
 
 llxFooter();
