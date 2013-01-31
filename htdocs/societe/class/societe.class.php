b29c4358a919ff7fb66843bc7d7b81f368982c88
Herve Prot <herve.prot@symeos.com>
2013-01-31 22:49:32 +0100

    Fix error in societe.class.php

--- htdocs/societe/class/societe.class.php
+++ htdocs/societe/class/societe.class.php
@@ -112,9 +112,6 @@ class Societe extends nosqlDocument {
     var $ref_int;
     var $import_key;
     var $logo;
-    var $logo_small;
-    var $logo_mini;
-    var $oldcopy;
 
     /**
      *    Constructor
@@ -126,6 +123,8 @@ class Societe extends nosqlDocument {
 
         $this->fk_extrafields = new ExtraFields($db);
         $this->fk_extrafields->fetch(get_class($this));
+        
+        $this->commercial_id = new stdClass();
 
         return 1;
     }
