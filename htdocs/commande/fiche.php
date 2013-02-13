d27ff89b18411410039adfd260058aa9bd427501
David MOOTHEN <dmoothen@websitti.fr>
2013-02-13 23:38:38 +0100

    Disabling old lines display in Commande.

--- htdocs/commande/fiche.php
+++ htdocs/commande/fiche.php
@@ -1413,42 +1413,42 @@ if (($action == 'create' || $action == 'edit') && $user->rights->commande->creer
     
 	$object->showLines();
 
-	print start_box($langs->trans('OrderLines'), "twelve", $object->fk_extrafields->ico, false);
-	print '<table id="tablelines" class="noborder" width="100%">';
-
-	$nbLines = count($object->lines);
-
-	// Show object lines
-	if (!empty($object->lines))
-		$ret = $object->printObjectLines($action, $mysoc, $soc, $lineid, 1);
-
-	// Form to add new line
-
-	if ($object->Status == "DRAFT" && $user->rights->commande->creer) {
-		if ($action != 'editline') {
-			$var = true;
-
-			if ($conf->global->MAIN_FEATURES_LEVEL > 1) {
-				// Add free or predefined products/services
-				$object->formAddObjectLine(1, $mysoc, $soc);
-			} else {
-				// Add free products/services
-				$object->formAddFreeProduct(1, $mysoc, $soc);
-
-				// Add predefined products/services
-				if (!empty($conf->product->enabled) || !empty($conf->service->enabled)) {
-					$var = !$var;
-					$object->formAddPredefinedProduct(1, $mysoc, $soc);
-				}
-			}
-
-			$parameters = array();
-			$reshook = $hookmanager->executeHooks('formAddObjectLine', $parameters, $object, $action);	// Note that $action and $object may have been modified by hook
-		}
-	}
-	print '</table>';
-	print end_box();
-
+//	print start_box($langs->trans('OrderLines'), "twelve", $object->fk_extrafields->ico, false);
+//	print '<table id="tablelines" class="noborder" width="100%">';
+//
+//	$nbLines = count($object->lines);
+//
+//	// Show object lines
+//	if (!empty($object->lines))
+//		$ret = $object->printObjectLines($action, $mysoc, $soc, $lineid, 1);
+//
+//	// Form to add new line
+//
+//	if ($object->Status == "DRAFT" && $user->rights->commande->creer) {
+//		if ($action != 'editline') {
+//			$var = true;
+//
+//			if ($conf->global->MAIN_FEATURES_LEVEL > 1) {
+//				// Add free or predefined products/services
+//				$object->formAddObjectLine(1, $mysoc, $soc);
+//			} else {
+//				// Add free products/services
+//				$object->formAddFreeProduct(1, $mysoc, $soc);
+//
+//				// Add predefined products/services
+//				if (!empty($conf->product->enabled) || !empty($conf->service->enabled)) {
+//					$var = !$var;
+//					$object->formAddPredefinedProduct(1, $mysoc, $soc);
+//				}
+//			}
+//
+//			$parameters = array();
+//			$reshook = $hookmanager->executeHooks('formAddObjectLine', $parameters, $object, $action);	// Note that $action and $object may have been modified by hook
+//		}
+//	}
+//	print '</table>';
+//	print end_box();
+//
 
 	if ($action != 'presend') {
 		print '<a name="builddoc"></a>'; // ancre
