8fff70b0ad34bd5b78c83c7dbf9fc3f8daae9d26
Herve Prot <herve.prot@symeos.com>
2013-02-18 13:07:31 +0100

    SPEED-1 Add test function for proxy_pass couchdb and couchdb version

--- htdocs/install/ajax/install.php
+++ htdocs/install/ajax/install.php
@@ -1,5 +1,6 @@
 <?php
-/* Copyright (C) 2013 Regis Houssin  <regis.houssin@capnetworks.com>
+/* Copyright (C) 2013 Regis Houssin		<regis.houssin@capnetworks.com>
+ * Copyright (C) 2013 Herve Prot		<herve.prot@symeos.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,16 +20,15 @@
  *       \file       htdocs/install/ajax/install.php
  *       \brief      File to get all status for install process
  */
-
 require '../inc.php';
 
-$default_lang = GETPOST('lang','alpha');
+$default_lang = GETPOST('lang', 'alpha');
 $langs->setDefaultLang($default_lang);
 
 $langs->load("install");
 $langs->load("errors");
 
-$action	= GETPOST('action','alpha');
+$action = GETPOST('action', 'alpha');
 $out = array();
 
 
@@ -41,13 +41,12 @@ header('Content-type: application/json');
 // This variable are loaded by inc.php
 // $main_couchdb_host
 // $main_couchdb_port
-
 // Create config file
 if ($action == 'create_config') {
-	$couchdb_host	= GETPOST('couchdb_host', 'alpha');
-	$couchdb_port	= GETPOST('couchdb_port', 'int');
-	$memcached_host	= GETPOST('memcached_host', 'alpha');
-	$memcached_port	= GETPOST('memcached_port', 'int');
+	$couchdb_host = GETPOST('couchdb_host', 'alpha');
+	$couchdb_port = GETPOST('couchdb_port', 'int');
+	$memcached_host = GETPOST('memcached_host', 'alpha');
+	$memcached_port = GETPOST('memcached_port', 'int');
 	// Save old conf file on disk
 	if (file_exists("$conffile")) {
 		// We must ignore errors as an existing old file may already exists and not be replacable or
@@ -63,8 +62,8 @@ if ($action == 'create_config') {
 
 // Create sync user
 } else if ($action == 'create_syncuser') {
-	$couchdb_user_sync	= GETPOST('couchdb_user_sync', 'alpha');
-	$couchdb_pass_sync	= GETPOST('couchdb_pass_sync', 'alpha');
+	$couchdb_user_sync = GETPOST('couchdb_user_sync', 'alpha');
+	$couchdb_pass_sync = GETPOST('couchdb_pass_sync', 'alpha');
 	// $main_couchdb_host
 	// $main_couchdb_port
 
@@ -73,7 +72,7 @@ if ($action == 'create_config') {
 
 // Create database
 } else if ($action == 'create_database') {
-	$couchdb_name	= GETPOST('couchdb_name', 'alpha');
+	$couchdb_name = GETPOST('couchdb_name', 'alpha');
 
 	$couch = new couchClient($main_couchdb_host . ':' . $main_couchdb_port . '/', $couchdb_name);
 
@@ -82,7 +81,8 @@ if ($action == 'create_config') {
 			$couch->createDatabase();
 			echo json_encode(array('status' => 'ok', 'value' => $langs->trans('DatabaseCreated')));
 		} catch (Exception $e) {
-			echo json_encode(array('status' => 'error'));
+			echo json_encode(array('status' => 'error', 'value' => $e->getMessage()));
+			error_log($e->getMessage());
 		}
 	} else {
 		echo json_encode(array('status' => 'ok', 'value' => $langs->trans('DatabaseAlreadyExists'))); // database already exists
@@ -90,19 +90,20 @@ if ($action == 'create_config') {
 
 // Populate database
 } else if ($action == 'populate_database') {
-	$filename	= GETPOST('filename', 'alpha');
-	$filepath	= GETPOST('filepath');
+	$filename = GETPOST('filename', 'alpha');
+	$filepath = GETPOST('filepath');
 	// $main_couchdb_host
 	// $main_couchdb_port
 
 	sleep(1); // for test
 	echo json_encode(array('status' => 'ok'));
 
+	/*
 // Create superadmin
 } else if ($action == 'create_admin') {
-	$couchdb_name		= GETPOST('couchdb_name', 'alpha');
-	$couchdb_user_root	= GETPOST('couchdb_user_root', 'alpha');
-	$couchdb_pass_root	= GETPOST('couchdb_pass_root', 'alpha');
+	$couchdb_name = GETPOST('couchdb_name', 'alpha');
+	$couchdb_user_root = GETPOST('couchdb_user_root', 'alpha');
+	$couchdb_pass_root = GETPOST('couchdb_pass_root', 'alpha');
 
 	$couch = new couchClient($main_couchdb_host . ':' . $main_couchdb_port . '/', $couchdb_name);
 	$admin = new couchAdmin($couch);
@@ -113,26 +114,26 @@ if ($action == 'create_config') {
 
 		$user = $admin->getUser($couchdb_user_root);
 
-		$user->Status			= 'ENABLE';
-		$user->entity			= $couchdb_name;
-		$user->admin			= true;
-		$user->superadmin		= true;
-		print_r($user); exit;
+		$user->Status = 'ENABLE';
+		$user->entity = $couchdb_name;
+		$user->admin = true;
+		$user->superadmin = true;
+		print_r($user);
+		exit;
 		$admin->client->storeDoc($user);
 
 		echo json_encode(array('status' => 'ok'));
-
 	} catch (Exception $e) {
 		echo json_encode(array('status' => 'error', 'value' => $e->getMessage()));
 	}
 
 // Create first user
 } else if ($action == 'create_user') {
-	$couchdb_user_firstname	= GETPOST('couchdb_user_firstname', 'alpha');
-	$couchdb_user_lastname	= GETPOST('couchdb_user_lastname', 'alpha');
-	$couchdb_user_pseudo	= GETPOST('couchdb_user_pseudo', 'alpha');
-	$couchdb_user_email		= GETPOST('couchdb_user_email', 'alpha');
-	$couchdb_user_pass		= GETPOST('couchdb_user_pass', 'alpha');
+	$couchdb_user_firstname = GETPOST('couchdb_user_firstname', 'alpha');
+	$couchdb_user_lastname = GETPOST('couchdb_user_lastname', 'alpha');
+	$couchdb_user_pseudo = GETPOST('couchdb_user_pseudo', 'alpha');
+	$couchdb_user_email = GETPOST('couchdb_user_email', 'alpha');
+	$couchdb_user_pass = GETPOST('couchdb_user_pass', 'alpha');
 	// $main_couchdb_host
 	// $main_couchdb_port
 
@@ -147,5 +148,8 @@ if ($action == 'create_config') {
 		echo json_encode(array('status' => 'ok', 'value' => $langs->trans('LockFileCreated')));
 	else
 		echo json_encode(array('status' => 'error', 'value' => $langs->trans('LockFileCouldNotBeCreated')));
+}
+	 * 
+	 */
 }
 ?>
