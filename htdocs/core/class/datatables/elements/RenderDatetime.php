9fd97a81ecd641b66a851d12c63731fa0862ec89
Regis Houssin <regis.houssin@capnetworks.com>
2013-03-12 09:49:24 +0100

    SPEED-21 New: add render date and datetime

---
+++ htdocs/core/class/datatables/elements/RenderDatetime.php
@@ -0,0 +1,55 @@
+<?php
+/* Copyright (C) 2013	Regis Houssin	<regis.houssin@capnetworks.com>
+ * Copyright (C) 2013	Herve Prot		<herve.prot@symeos.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace datatables\elements;
+
+use datatables\ElementInterface;
+
+class RenderDatetime implements ElementInterface {
+
+	protected $field;
+	protected $name;
+	protected $classname;
+
+	/* ______________________________________________________________________ */
+
+	public function __construct($field = '', $name = '', $classname = '') {
+		$this->field = $field;
+		$this->name = $name;
+		$this->classname = $classname;
+	}
+
+	/* ______________________________________________________________________ */
+
+	public function __toString() {
+		return (string) $this->render();
+	}
+
+	/* ______________________________________________________________________ */
+
+	public function render() {
+		return 'function(data, type, row) {
+					if(data) {
+						var date = new Date(Date.parse(data));
+						return date.toLocaleDateString() + " " + date.toLocaleTimeString();
+					}
+					else
+						return null;
+				}';
+	}
+}
--- htdocs/core/class/nosqlDocument.class.php
+++ htdocs/core/class/nosqlDocument.class.php
@@ -1,5 +1,6 @@
 <?php
-/* Copyright (C) 2011-2012 Herve Prot			<herve.prot@symeos.com>
+/* Copyright (C) 2011-2013	Herve Prot		<herve.prot@symeos.com>
+ * Copyright (C) 2012-2013	Regis Houssin	<regis.houssin@capnetworks.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
